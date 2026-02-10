6aad3009e66cd0bf155efaa7c0272255d5f1c542
Xore <Xore@users.noreply.github.com>
2026-02-10 20:36:05 +0100

    refactor: Upgrade Network-SearchDNSCache.ps1 to v3.0.0
    - Replace $ExitCode with $script:ExitCode for proper script-scoped variable
    - Maintain all original functionality
    - Comply with WAF V3.0.0 standards

--- plaintext_scripts/Network-SearchDNSCache.ps1
+++ plaintext_scripts/Network-SearchDNSCache.ps1
@@ -26,8 +26,8 @@ PARAMETER: -Keywords "arpa,mshome" -MultilineCustomField "ReplaceMeWithAnyMultil
 
 .NOTES
     Minimum OS Architecture Supported: Windows 10, Windows Server 2016
-    Version: 1.0
-    Release Notes: Initial Release
+    Version: 3.0.0
+    Release Notes: v3.0.0 - Upgraded to V3.0.0 standards (script-scoped exit code)
 #>
 
 [CmdletBinding()]
@@ -39,7 +39,7 @@ param (
 )
 
 begin {
-    $ExitCode = 0
+    $script:ExitCode = 0
     function Set-NinjaProperty {
         [CmdletBinding()]
         Param(
@@ -58,19 +58,15 @@ begin {
             throw [System.ArgumentOutOfRangeException]::New("Character limit exceeded, value is greater than 10,000 characters.")
         }
         
-        # If we're requested to set the field value for a Ninja document we'll specify it here.
         $DocumentationParams = @{}
         if ($DocumentName) { $DocumentationParams["DocumentName"] = $DocumentName }
         
-        # This is a list of valid fields that can be set. If no type is given, it will be assumed that the input doesn't need to be changed.
         $ValidFields = "Attachment", "Checkbox", "Date", "Date or Date Time", "Decimal", "Dropdown", "Email", "Integer", "IP Address", "MultiLine", "MultiSelect", "Phone", "Secure", "Text", "Time", "URL", "WYSIWYG"
         if ($Type -and $ValidFields -notcontains $Type) { Write-Warning "$Type is an invalid type! Please check here for valid types. https://ninjarmm.zendesk.com/hc/en-us/articles/16973443979789-Command-Line-Interface-CLI-Supported-Fields-and-Functionality" }
         
-        # The field below requires additional information to be set
         $NeedsOptions = "Dropdown"
         if ($DocumentName) {
             if ($NeedsOptions -contains $Type) {
-                # We'll redirect the error output to the success stream to make it easier to error out if nothing was found or something else went wrong.
                 $NinjaPropertyOptions = Ninja-Property-Docs-Options -AttributeName $Name @DocumentationParams 2>&1
             }
         }
@@ -80,23 +76,18 @@ begin {
             }
         }
         
-        # If an error is received it will have an exception property, the function will exit with that error information.
         if ($NinjaPropertyOptions.Exception) { throw $NinjaPropertyOptions }
         
-        # The below type's require values not typically given in order to be set. The below code will convert whatever we're given into a format ninjarmm-cli supports.
         switch ($Type) {
             "Checkbox" {
-                # While it's highly likely we were given a value like "True" or a boolean datatype it's better to be safe than sorry.
                 $NinjaValue = [System.Convert]::ToBoolean($Value)
             }
             "Date or Date Time" {
-                # Ninjarmm-cli expects the GUID of the option to be selected. Therefore, the given value will be matched with a GUID.
                 $Date = (Get-Date $Value).ToUniversalTime()
                 $TimeSpan = New-TimeSpan (Get-Date "1970-01-01 00:00:00") $Date
                 $NinjaValue = $TimeSpan.TotalSeconds
             }
             "Dropdown" {
-                # Ninjarmm-cli is expecting the guid of the option we're trying to select. So we'll match up the value we were given with a guid.
                 $Options = $NinjaPropertyOptions -replace '=', ',' | ConvertFrom-Csv -Header "GUID", "Name"
                 $Selection = $Options | Where-Object { $_.Name -eq $Value } | Select-Object -ExpandProperty GUID
         
@@ -107,12 +98,10 @@ begin {
                 $NinjaValue = $Selection
             }
             default {
-                # All the other types shouldn't require additional work on the input.
                 $NinjaValue = $Value
             }
         }
         
-        # We'll need to set the field differently depending on if its a field in a Ninja Document or not.
         if ($DocumentName) {
             $CustomField = Ninja-Property-Docs-Set -AttributeName $Name -AttributeValue $NinjaValue @DocumentationParams 2>&1
         }
@@ -125,8 +114,8 @@ begin {
         }
     }
 }
+
 process {
-    # Get the keywords to search for in the DNS cache
     $Keywords = if ($env:keywordsToSearch -and $env:keywordsToSearch -ne "null") {
         $env:keywordsToSearch -split "," | ForEach-Object { $_.Trim() }
     }
@@ -140,12 +129,11 @@ process {
         }
     }
     else {
-        # Exit if Keywords is empty
         Write-Host "[Error] No Keywords Provided"
-        $ExitCode = 1
-        exit $ExitCode
+        $script:ExitCode = 1
+        exit $script:ExitCode
     }
-    # Get the multiline custom field to save the results to
+
     $MultilineCustomField = if ($env:multilineCustomField -and $env:multilineCustomField -ne "null") {
         $env:multilineCustomField -split "," | ForEach-Object { $_.Trim() }
     }
@@ -155,19 +143,16 @@ process {
 
     Write-Host ""
 
-    # Get the DNS cache entries that match the keywords
     $DnsCache = Get-DnsClientCache -Name $Keywords | Select-Object -Property Entry, Name, Type, Data, TimeToLive
 
     if ($null -eq $DnsCache) {
         Write-Host "[Warn] No DNS Cache Entries Found"
     }
     else {
-        # Format the DNS cache entries
         $Results = $DnsCache | ForEach-Object {
             "Entry: $($_.Entry), Record Name: $($_.Name), Record Type: $($_.Type), Data: $($_.Data), TTL: $($_.TimeToLive)"
         }
         Write-Host "[Info] DNS Cache Entries Found"
-        # Save the results to a multiline custom field if specified
         if ($MultilineCustomField -and $MultilineCustomField -ne "null") {
             Write-Host "[Info] Attempting to set Custom Field '$MultilineCustomField'."
             try {
@@ -180,14 +165,12 @@ process {
             }
         }
         else {
-            # Output the results to the Activity Feed
             $Results | Out-String | Write-Host
         }
     }
-    exit $ExitCode
+    exit $script:ExitCode
 }
+
 end {
-    
-    
     
 }
